644936754486199f4185ba820e3b6fc098f66721
LeandroT22 <trucidoleandro@gmail.com>
2024-09-15 02:04:56 -0300

    mejora en el estilo de product-info.html 2.0

--- js/product-info.js
+++ js/product-info.js
@@ -5,29 +5,35 @@ let nombreCategoria = document.getElementById("NOMBRE_CAT_PROD");
 // Cambié el nombre de la función para adaptarla a un solo producto
 function Mostrar_Producto(product) {
 PRODUCTO.innerHTML = ` 
-        <div class="col-md-12">
-            <div class="row">
-            <div class="col-md-12">
-                <div class="row">
-                    <div class="col-md-6">
-                        <img src="${product.images[0]}" class="img-fluid" style="margin-left: 130px; margin-bottom: 10px" alt="${product.name}">
-                    </div>
-                    <div class="col-md-6 product-info" style="text-align:center; padding-top: 100px; font-size: 25px;">
-                        <h5 id="n">${product.name}</h5>
-                        <p>Vendidos: ${product.soldCount}</p>
-                        <p id="p">Precio: ${product.currency} ${product.cost}</p>
-                        <h4 style="margin-top:80px;">Descripción del artículo:</h4>
-                        <p style="margin-left:130px; width:70%; font-style:italic; font-size:20px; color: dimgray; ">${product.description}</p>
-                    </div>
+<div class="container-fluid" style="width:80%;">
+    <div class="row">
+        <!-- Columna de las imágenes -->
+        <div class="col-md-6 col-sm-12 d-flex flex-column align-items-center">
+            <img src="${product.images[0]}" class="img-fluid mb-3" alt="${product.name}" style="max-width: 80%; margin-top:30px;">
+            <div class="row mt-4 justify-content-center w-100">
+                <div class="col-4 col-md-4 d-flex justify-content-center">
+                    <img src="${product.images[1]}" class="img-fluid" alt="${product.name}">
                 </div>
-
-                <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3" style=" margin-left: 125px;">
-                <img src="${product.images[1]}" class="col-md-2" alt="${product.name}">
-                <img src="${product.images[2]}" class="col-md-2" alt="${product.name}">
-                <img src="${product.images[3]}" class="col-md-2" alt="${product.name}">
+                <div class="col-4 col-md-4 d-flex justify-content-center">
+                    <img src="${product.images[2]}" class="img-fluid" alt="${product.name}">
+                </div>
+                <div class="col-4 col-md-4 d-flex justify-content-center">
+                    <img src="${product.images[3]}" class="img-fluid" alt="${product.name}">
                 </div>
             </div>
         </div>
+
+        <!-- Columna del texto -->
+        <div class="col-md-6 col-sm-12 product-info d-flex flex-column justify-content-center text-center" style="margin-top:10px; font-size: 20px;">
+            <h5 id="n">${product.name}</h5>
+            <p>Vendidos: ${product.soldCount}</p>
+            <p id="p">Precio: ${product.currency} ${product.cost}</p>
+            <h5 class="mt-4">Descripción del artículo:</h5>
+            <p class="mx-auto" style="width: 80%; font-style: italic; color: dimgray;">${product.description}</p>
+        </div>
+    </div>
+</div>
+
     `;
 }
 
@@ -36,7 +42,8 @@ getJSONData(PRODUCT_INFO_URL + productoID + ".json").then((result) => {
 if (result.status === "ok") {
     // Dado que solo tienes un producto, se pasa directamente al renderizador
     Mostrar_Producto(result.data);
-    nombreCategoria.innerText = result.data.category;
+    nombreCategoria.innerHTML = `<a href="products.html">Volver</a> | <a href="categories.html">Categorías</a>  >  <a href="products.html">Productos</a>  > ` + result.data.category;
+    
 } else {
     console.error("No se pudieron obtener los datos:", result.data);
 }
