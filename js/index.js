c332957d122411459d356996f5833a925bc453dc
vickyylara08 <vickylaratorena1@gmail.com>
2024-10-17 20:50:04 -0300

    menú desplegable

--- js/index.js
+++ js/index.js
@@ -13,22 +13,3 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    //nombre de usuario localStorage
-    const nombreUsuario = localStorage.getItem("user");
-    
-    // Verificar si hay un usuario autenticado
-    if (nombreUsuario) {
-        // Cambiar el texto del botón para mostrar el nombre del usuario
-        document.getElementById("nombreUsuarioBtn").textContent = nombreUsuario;
-    } else {
-        document.getElementById("nombreUsuarioBtn").textContent = "Invitado";
-    }
-});
-
-// Función de cierre de sesión
-function logout() {
-    localStorage.removeItem("isLoggedIn");
-    localStorage.removeItem("user");
-    window.location.href = "login.html"; // Redirigir a la página de login
-}
